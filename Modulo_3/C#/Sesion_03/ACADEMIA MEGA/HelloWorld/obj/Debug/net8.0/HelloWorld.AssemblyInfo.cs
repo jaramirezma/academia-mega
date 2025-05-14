9e1f46cb57b7b21c666fe28f0fbc8c72829ff471
jaramirezma <jaramirezma@megacable.com.mx>
2025-05-14 09:35:59 -0600

    Clase 03 | C#

--- Modulo_3/C#/Sesion_03/ACADEMIA MEGA/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ Modulo_3/C#/Sesion_03/ACADEMIA MEGA/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abe494cc356c70f954ddefd53f63666fcaea7244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cbfde8f228e07e9272c3011bfd9a2806d9800f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

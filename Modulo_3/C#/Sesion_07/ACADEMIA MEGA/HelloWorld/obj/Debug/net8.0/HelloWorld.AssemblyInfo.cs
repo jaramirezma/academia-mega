a53d7955af68d6915329e14c858e472a3b9054da
jaramirezma <jaramirezma@megacable.com.mx>
2025-05-20 09:31:20 -0600

    Clase 07 | C#

--- Modulo_3/C#/Sesion_07/ACADEMIA MEGA/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ Modulo_3/C#/Sesion_07/ACADEMIA MEGA/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee098c81fc1eafba005c34868f72cd963bacd202")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebfd4202623d38d7f5acde3a3bf92d6cecfd90b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Modulo_3/C#/Sesion_08/ACADEMIA MEGA/PrimeraAPI/ProductosController.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.Versioning;
+using PrimeraAPI.Models;
+using PrimeraAPI.Data;
+
+namespace PrimeraAPI.controllers
+{
+    [ApiController]
+    //[ApiVersion("1.0")] v{version:apiVersion}/
+    [Route("api/[controller]")] // api/v1
+
+    public class ProductosController : ControllerBase
+    {
+        //Aquí sería la dectura de datos en BD
+        private readonly ProductoService _service;
+        public ProductosController(ProductoService service)
+        {
+            _service = service;
+        }
+
+
+        /*
+            - READ -
+        */
+
+        [HttpGet] // GET api/productos
+        public async Task<IActionResult> GetAll()
+        {
+            var lista = await _service.GetAllAsync();
+
+            return Ok(lista);
+        }
+
+
+        [HttpGet("{id}")] // GET api/productos/1
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        /*
+            - CREATE -
+        */
+
+
+        [HttpPost]  // POST api/productos
+        public async Task<IActionResult> Create(Producto nuevo)
+        {
+            var created = await _service.CreateAsync(nuevo);
+            return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+        }
+
+
+        /*
+            - UPDATE -
+        */
+
+        [HttpPut("{id}")] // PUT api/productos/1
+        public async Task<IActionResult> Update(int id, Producto actualizado)
+        {
+            var updated = await _service.UpdateAsync(id, actualizado);
+            if (!updated) return NotFound();
+            return NoContent();
+        }
+
+        /*
+            - DELETE -
+        */
+
+        [HttpDelete("{id}")] // DELETE api/productos/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleted = await _service.DeleteAsync(id);
+            if (!deleted) return NotFound();
+            return NoContent();
+        }
+
+    }
+}

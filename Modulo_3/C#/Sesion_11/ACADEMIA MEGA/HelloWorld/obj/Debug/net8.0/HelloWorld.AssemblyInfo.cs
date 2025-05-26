c100f7ca8346294de4695aee523b3407b43b1475
jaramirezma <jaramirezma@megacable.com.mx>
2025-05-26 16:55:34 -0600

    Eliminando bin, obj y appsettings.json de ACADEMIA MEGA

--- Modulo_3/C#/Sesion_11/ACADEMIA MEGA/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ Modulo_3/C#/Sesion_11/ACADEMIA MEGA/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf89de302be5640c2caf899fb08bc392bd05f38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d027963b23ff01a3c3fdd5a296ef6923c186ffdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

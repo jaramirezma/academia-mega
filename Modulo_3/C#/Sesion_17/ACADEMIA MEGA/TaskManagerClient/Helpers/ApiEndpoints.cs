852d61ad69540466e5d24bec1f0add9f9f4ae17c
jaramirezma <jaramirezma@megacable.com.mx>
2025-06-03 09:58:51 -0600

    Clase 17 | C#

---
+++ Modulo_3/C#/Sesion_17/ACADEMIA MEGA/TaskManagerClient/Helpers/ApiEndpoints.cs
@@ -0,0 +1,7 @@
+namespace TaskManagerClient.Helpers;
+
+public static class ApiEndpoints
+{
+    public const string Base = "http://localhost:5145/api";
+    public const string Tasks = $"{Base}/Tasks";
+}

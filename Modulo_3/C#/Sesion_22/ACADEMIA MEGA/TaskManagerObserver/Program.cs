eda6b6c39047f6c0d22af391e75887a31975678e
jaramirezma <jaramirezma@megacable.com.mx>
2025-06-11 08:29:04 -0600

    Clase 22 | C#

---
+++ Modulo_3/C#/Sesion_22/ACADEMIA MEGA/TaskManagerObserver/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using TaskManager.Shared.Events;
+
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("http://localhost:5145/taskEvents") //Cambiar por mi puerto
+    .Build();
+
+connection.On<TaskEvent>("TaskEvetn", ev =>
+{
+    Console.WriteLine($"{ev.EventName}: {ev.PayLoad.Title}");
+});
+
+await connection.StartAsync();
+Console.WriteLine("Observando eventos...");
+await Task.Delay(Timeout.Infinite); //-1 Es lo mismo que el Timeout.Infinite

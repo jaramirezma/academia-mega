c22ea9706bc7b5497e2302da43cf37ee95afdfb3
jaramirezma <jaramirezma@megacable.com.mx>
2025-06-12 09:40:51 -0600

    Clase 24 | C#

---
+++ Modulo_3/C#/Sesion_24/ACADEMIA MEGA/TiendaMVC/TiendaMvc.Tests/Models/ModelValidationTests.cs
@@ -0,0 +1,22 @@
+using Xunit;
+using FluentAssertions;
+using TiendaMVC.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class ModelValidationTests
+{
+    [Theory]
+    [InlineData("", "pwd")] //Simulando un usuario vacio
+    [InlineData("user", "")] //Simulando contraseña vacia
+    public void User_Should_Fail_If_Fields_Missing(string user, string pwd)
+    {
+        var dto = new User { Username = user, Password = pwd };
+
+        var results = new List<ValidationResult>();
+        var context = new ValidationContext(dto);
+        var valid = Validator.TryValidateObject(dto, context, results, true);
+
+        valid.Should().BeFalse();
+        results.Should().NotBeEmpty();
+    }
+}

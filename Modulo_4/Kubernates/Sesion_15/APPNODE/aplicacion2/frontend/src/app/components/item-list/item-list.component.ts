a1fcff958293417cb0700bec25875d34fc6fbe3d
jaramirezma <jaramirezma@megacable.com.mx>
2025-07-14 08:15:37 -0600

    Clase 15 | Kubernetes

---
+++ Modulo_4/Kubernates/Sesion_15/APPNODE/aplicacion2/frontend/src/app/components/item-list/item-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemService, Item } from '../../services/item.service';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http'; 
+
+
+@Component({
+  selector: 'app-item-list', 
+  standalone: true,
+  imports: [NgFor, FormsModule, HttpClientModule],
+  templateUrl: './item-list.component.html' 
+})
+
+export class ItemListComponent implements OnInit {
+  items: Item[] = [];
+  newItem: Item = { name: '', price: 0 };
+
+  constructor(private itemSvc: ItemService) {}
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  load() {
+    this.itemSvc.getAll().subscribe(data => this.items = data);
+  }
+
+  add() {
+    this.itemSvc.create(this.newItem).subscribe(() => {
+      this.newItem = { name: '', price: 0 };
+      this.load();
+    });
+  }
+}

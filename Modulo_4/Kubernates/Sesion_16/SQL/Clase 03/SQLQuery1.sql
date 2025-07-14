b5e5dc506962abc98ca650dd3020f8bf9515b98d
jaramirezma <jaramirezma@megacable.com.mx>
2025-07-14 09:37:47 -0600

    Clase 16 | Kubernetes

---
+++ Modulo_4/Kubernates/Sesion_16/SQL/Clase 03/SQLQuery1.sql
@@ -0,0 +1,10 @@
+/*
+CREATE DATABASE Starbucks;
+
+CREATE LOGIN jaramirezmaCurso WITH PASSWORD = 'Nayarit.0106*';
+
+*/
+
+USE Starbucks;
+GO
+CREATE USER jaramirezmaCurso FOR LOGIN jaramirezmaCurso;
